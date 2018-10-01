22531688ff8f8b69187e4cad1c099c2deb9c46ae
Fabio Rodrigues de la Rocha <fabio.rocha@gmail.com>
2018-10-01 16:38:28 -0300

    teclado ps2

--- Unidade3/Codigos/experiencia_teclado_ps2/teclado.cpp
+++ Unidade3/Codigos/experiencia_teclado_ps2/teclado.cpp
@@ -22,6 +22,38 @@ unsigned char conversao (unsigned char cod)
 		case 0x32:  return 'B';
 		case 0x21:  return 'C';
         case 0x23:  return 'D';
+        case 0x24:  return 'E';
+        case 0x2B:  return 'F';
+        case 0x34:  return 'G';
+        case 0x33:  return 'H';
+        case 0x43:  return 'I';
+		case 0x3B:  return 'J';
+		case 0x42:  return 'K';
+        case 0x4B:  return 'L';
+		case 0x3A:  return 'M';
+		case 0x31:  return 'N';
+		case 0x44:  return 'O';
+		case 0x4D:  return 'P';
+		case 0x15:  return 'Q';
+        case 0x2D:  return 'R';
+        case 0x1B:  return 'S';
+        case 0x2C:  return 'T';
+        case 0x3C:  return 'U';
+        case 0x2A:  return 'V';
+        case 0x1D:  return 'W';
+        case 0x22:  return 'X';
+        case 0x35:  return 'Y';
+        case 0x1A:  return 'Z';
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
 
 					
 	}
